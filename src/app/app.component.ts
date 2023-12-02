c20f185dadbc7e4d1730253f0ebc63eecfe507c4
EdisonAmendano <edison32314@gmail.com>
2023-12-01 20:49:14 -0500

    Proyecto Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './page/home/home.component';
 
 @Component({
   selector: 'app-root',
